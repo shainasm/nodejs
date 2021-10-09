20a514c36751b791ff8e0d66ec35e6626a262d99
shainasm <smulani2002@google.com>
2021-10-09 07:48:58 -0700

    nodejs intro

---
+++ app.js
@@ -0,0 +1,10 @@
+const { add, subtract, divide, multiply, message} =
+require('./utlis')
+
+add()
+subtract()
+divide()
+multiply()
+console.log(message)
+
+// put replaces and patch updates
---
+++ index.js
@@ -0,0 +1,6 @@
+// module is a set of code or just a file
+const greet=()=>'hi'
+
+module.exports.greet= greet
+
+console.log('i am index.js')
