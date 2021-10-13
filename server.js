5dc8e5a9393ff3c66fe49fe78ea75b750ba02d18
shainasm <smulani2002@google.com>
2021-10-13 08:33:56 -0700

    thunder client

--- server.js
+++ server.js
@@ -1,42 +1,14 @@
-// const http = require('http')
-// const fs = require('fs')
-// const path = require('path')
-
-// const server = http.createServer((request, response) => {
-//     // request.url
-//     // request module is used to make http calls
-//     const { url } = request
-//     if (url == '/login') {
-//         response.write('<h1>LOGIN</h1>')
-//         // we created a new route login
-//         response.end()
-//     }
-//     if (url == '/signup') {
-//         response.write('<h1>SINGUP</h1>')
-//         // new route signup
-//         response.end()
-//     }
-// })
-
-
-// server.listen(3000, () => {
-//     console.log(`Server listening at PORT: ${3000}`)
-// })
-
-
 const express = require('express')
+const PORT = 3000
 const app = express()
 
-app.get('/login',(req,res)=>{
-  console.log(req.url)
-  res.send('this is a login')
-})
+app.use(express.json())
 
 app.get('/signup',(req,res)=>{
-  console.log(req.url)
-  res.send('<h1>this is a signup</h1>')
+  console.log(req.body)
+  res.send('dadada')
 })
 
-app.listen(3000, ()=> {
-  console.log("server listening at port 3000")
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`)
 })
