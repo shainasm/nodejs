226bb4a5fdb37c83185f0743c650218c45a6433b
shainasm <smulani2002@google.com>
2021-10-14 08:01:19 -0700

    get and post route

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express')
-const PORT = 3000
 const app = express()
+const PORT = 3000
 
 app.use(express.json())
 
-app.get('/signup',(req,res)=>{
-  console.log(req.body)
-  res.send('dadada')
+let products = [{ name: 'iphone 13 case', price: '999' }, { name: 'iphone 12 case', price: '1199'}, { name: 'iphone 13 pro case', price: '14999'}]
+
+const validator = (req,res,next)=>{
+  const {name,price} = req.body
+
+  if (!name && !price) res.json({ error: "validation failed"})
+  else next()
+}
+
+// -------------PUBLIC ROUTES-----------------
+// GET ROUTE
+// SEND ALL PRODUCTS
+app.get('/products',(req,res)=>{
+  res.json({ products })
+})
+
+// --------------PRIVATE ROUTES-----------------
+app.post('/products/add',validator,(req,res)=>{
+ const { name,price } = req.body
+ 
+  products.push({
+    name,
+    price,
+  })
+
+  res.send({ products })
 })
 
 app.listen(PORT, () => {
