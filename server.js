bd9b5dc1f64c8e7215b2bf0a1d61de33b7ccc594
shainasm <smulani2002@google.com>
2021-10-18 08:35:09 -0700

    .

--- server.js
+++ server.js
@@ -21,4 +21,10 @@ app.listen(3001, () => {
     console.log("Listening at PORT 3001")
 })
 
+
+
+
+
+
+
 // in try catch if the try block failes due to some  then catch block is going to be executed and server is not going to be crashed
