3b13e9151d4c28e0f5fbc9c702807e7a6f4edf5d
shainasm <smulani2002@google.com>
2021-10-25 07:38:46 -0700

    nothing much just added 2 html files

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express')
-const database = require('./database/db')
 const app = express()
 const categoryRoutes = require('./routes/categoryRoutes')
 
 app.use(express.json())
+app.use(express.static('public'))
 
-app.use('/category',categoryRoutes)
+app.get('/', (req,res)=> {
+    res.sendFile('index.html')
+})
 
 app.listen(3001, () => {
     console.log("Listening at PORT 3001")
 })
 
 
-// in try catch if the try block failes due to some  then catch block is going to be executed and server is not going to be crashed
